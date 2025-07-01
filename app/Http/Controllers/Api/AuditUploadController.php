9a8fca161ded079b748857d0758f07c39a6527a8
komys <komysobeh@gmail.com>
2025-07-01 11:08:19 -0400

    pdf to text

--- app/Http/Controllers/Api/AuditUploadController.php
+++ app/Http/Controllers/Api/AuditUploadController.php
@@ -50,7 +50,7 @@ class AuditUploadController extends Controller
 
             Log::info('PDF uploaded: ' . $fullPath);
 
-            $pdfToolPath = config('pdf.pdftotext_path', 'C:\poppler\poppler-24.08.0\Library\bin\pdftotext.exe');
+            $pdfToolPath = config('pdf.pdftotext_path', '/usr/bin/pdftotext');
             $rawText = Pdf::getText($fullPath, $pdfToolPath);
 
             if (empty($rawText)) {
